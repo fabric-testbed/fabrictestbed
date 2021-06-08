d9acf842dca194107dcd3fd15051bad5cd1a75f2
Komal Thareja <kthare10@renci.org>
2021-06-08 17:39:11 -0400

    update fim imports

--- fabrictestbed/slice_editor/__init__.py
+++ fabrictestbed/slice_editor/__init__.py
@@ -2,14 +2,16 @@ import fim.user.topology as topo
 import fim.user.node as node
 import fim.user.link as link
 import fim.user.component as comp
-import fim.user.switch_fabric as sf
+import fim.user.network_service as ns
 import fim.slivers.network_node as nnode
 import fim.slivers.network_link as nlink
+import fim.slivers.network_service as nss
 import fim.slivers.attached_components as atcomp
 import fim.slivers.component_catalog as cata
 import fim.slivers.interface_info as iinfo
 import fim.slivers.capacities_labels as caplab
 import fim.slivers.delegations as dlg
+import fim.graph.abc_property_graph as abcpg
 
 # push some definitions up to simplify import management
 
@@ -23,13 +25,18 @@ ComponentType = atcomp.ComponentType
 ComponentCatalog = cata.ComponentCatalog
 InterfaceType = iinfo.InterfaceType
 Capacities = caplab.Capacities
+CapacityHints = caplab.CapacityHints
 Labels = caplab.Labels
 ReservationInfo = caplab.ReservationInfo
-Layer = sf.SFLayer
+Layer = nss.NSLayer
 Link = link.Link
 LinkType = nlink.LinkType
+ServiceType = ns.ServiceType
+NetworkService = ns.NetworkService
 TopologyDetail = topo.TopologyDetail
 Delegation = dlg.Delegation
+Delegations = dlg.Delegations
 DelegationType = dlg.DelegationType
 Pool = dlg.Pool
-ARMPools = dlg.ARMPools
+Pools = dlg.Pools
+GraphFormat = abcpg.GraphFormat
