9ca491993090dbb9fc08851d4adc4b54ca9eb5d2
Komal Thareja <kthare10@renci.org>
2023-04-27 09:32:15 -0400

    add support for delete all slices for a user within a project

--- fabrictestbed/slice_manager/slice_manager.py
+++ fabrictestbed/slice_manager/slice_manager.py
@@ -256,17 +256,19 @@ class SliceManager:
             self.__load_tokens()
         return self.oc_proxy.modify_accept(token=self.get_id_token(), slice_id=slice_id)
 
-    def delete(self, *, slice_object: Slice) -> Tuple[Status, Union[Exception, None]]:
+    def delete(self, *, slice_object: Slice, email: str = None) -> Tuple[Status, Union[Exception, None]]:
         """
         Delete slice(s)
         @param slice_object slice to be deleted
+        @param email user's email
         @return Tuple containing Status and Exception/Json containing deletion status
         """
-        if slice_object is None or not isinstance(slice_object, Slice):
-            return Status.INVALID_ARGUMENTS, SliceManagerException("Invalid arguments - slice_object")
+        if email is None and (slice_object is None or not isinstance(slice_object, Slice)):
+            return Status.INVALID_ARGUMENTS, SliceManagerException(f"Invalid arguments - "
+                                                                   f"slice_object: {slice_object}/email: {email}")
         if self.__should_renew():
             self.__load_tokens()
-        return self.oc_proxy.delete(token=self.get_id_token(), slice_id=slice_object.slice_id)
+        return self.oc_proxy.delete(token=self.get_id_token(), slice_id=slice_object.slice_id, email=email)
 
     def slices(self, includes: List[SliceState] = None, excludes: List[SliceState] = None, name: str = None,
                limit: int = 20, offset: int = 0, slice_id: str = None) -> Tuple[Status, Union[Exception, List[Slice]]]:
