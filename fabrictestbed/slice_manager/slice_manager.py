87143cf865e0b1ce4bb824dfd162f5050031d0c2
Komal Thareja <kthare10@renci.org>
2023-04-27 09:42:01 -0400

    make slice_object optional

--- fabrictestbed/slice_manager/slice_manager.py
+++ fabrictestbed/slice_manager/slice_manager.py
@@ -256,7 +256,7 @@ class SliceManager:
             self.__load_tokens()
         return self.oc_proxy.modify_accept(token=self.get_id_token(), slice_id=slice_id)
 
-    def delete(self, *, slice_object: Slice, email: str = None) -> Tuple[Status, Union[Exception, None]]:
+    def delete(self, *, slice_object: Slice = None, email: str = None) -> Tuple[Status, Union[Exception, None]]:
         """
         Delete slice(s)
         @param slice_object slice to be deleted
@@ -268,7 +268,8 @@ class SliceManager:
                                                                    f"slice_object: {slice_object}/email: {email}")
         if self.__should_renew():
             self.__load_tokens()
-        return self.oc_proxy.delete(token=self.get_id_token(), slice_id=slice_object.slice_id, email=email)
+        slice_id = slice_object.slice_id if slice_object is not None else None
+        return self.oc_proxy.delete(token=self.get_id_token(), slice_id=slice_id, email=email)
 
     def slices(self, includes: List[SliceState] = None, excludes: List[SliceState] = None, name: str = None,
                limit: int = 20, offset: int = 0, slice_id: str = None) -> Tuple[Status, Union[Exception, List[Slice]]]:
