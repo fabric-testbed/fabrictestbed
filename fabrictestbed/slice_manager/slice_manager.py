ed0f467f66275c2d605e8f3906c2945c915f7c48
Komal Thareja <kthare10@renci.org>
2023-06-08 10:44:00 -0400

    poa swagger updates and fim update

--- fabrictestbed/slice_manager/slice_manager.py
+++ fabrictestbed/slice_manager/slice_manager.py
@@ -26,10 +26,11 @@
 import json
 import os
 from datetime import datetime, timedelta
-from typing import Tuple, Union, List, Any
+from typing import Tuple, Union, List, Any, Dict
 
 import paramiko
 from fabric_cf.orchestrator.swagger_client import Sliver, Slice
+from fabric_cf.orchestrator.swagger_client.models import PoaData
 
 from fabrictestbed.slice_editor import ExperimentTopology, AdvertisedTopology, Node, GraphFormat
 from fabrictestbed.slice_manager import CredmgrProxy, OrchestratorProxy, CmStatus, Status, SliceState
@@ -345,6 +346,40 @@ class SliceManager:
 
         return self.oc_proxy.renew(token=self.get_id_token(), slice_id=slice_object.slice_id,
                                    new_lease_end_time=new_lease_end_time)
+
+    def poa(self, *, sliver_id: str, operation: str, vcpu_cpu_map: List[Dict[str, str]] = None,
+            node_set: List[str] = None) ->Tuple[Status, Union[Exception, List[PoaData]]]:
+        """
+        Issue POA for a sliver
+        @param sliver_id sliver Id for which to trigger POA
+        @param operation operation
+        @param vcpu_cpu_map list of mappings from virtual CPU to physical cpu
+        @param node_set list of the numa nodes
+        @return Tuple containing Status and POA information
+       """
+        if sliver_id is None or operation is None:
+            return Status.INVALID_ARGUMENTS, SliceManagerException("Invalid arguments - sliver_id or operation")
+
+        if self.__should_renew():
+            self.__load_tokens()
+
+        return self.oc_proxy.poa(token=self.get_id_token(), sliver_id=sliver_id, operation=operation,
+                                 vcpu_cpu_map=vcpu_cpu_map, node_set=node_set)
+
+    def get_poas(self, sliver_id: str = None, poa_id: str = None, limit: int = 20,
+                 offset: int = 0, ) -> Tuple[Status, Union[Exception, List[PoaData]]]:
+        """
+        Get POAs
+        @param sliver_id sliver Id for which to trigger POA
+        @param limit maximum number of slices to return
+        @param offset offset of the first slice to return
+        @param poa_id POA id identifying the POA
+        @return Tuple containing Status and POA information
+        """
+        if self.__should_renew():
+            self.__load_tokens()
+        return self.oc_proxy.get_poas(token=self.get_id_token(), limit=limit, offset=offset, sliver_id=sliver_id,
+                                      poa_id=poa_id)
 
     @staticmethod
     def __get_ssh_client() -> paramiko.SSHClient():
