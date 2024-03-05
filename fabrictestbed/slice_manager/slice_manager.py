b0c511353e00627e70ca7ff8f6328d7cebcfe3cb
Komal Thareja <kthare10@renci.org>
2024-03-06 01:39:41 +0530

    validate refresh token only for renewable tokens and when auto refresh is enabled

--- fabrictestbed/slice_manager/slice_manager.py
+++ fabrictestbed/slice_manager/slice_manager.py
@@ -155,10 +155,10 @@ class SliceManager:
         else:
             # First time login, use environment variable to load the tokens
             refresh_token = os.environ.get(Constants.CILOGON_REFRESH_TOKEN)
-        if refresh_token is None:
-            raise SliceManagerException(f"Unable to refresh tokens: no refresh token found!")
         # Renew the tokens to ensure any project_id changes are taken into account
         if refresh and self.auto_refresh:
+            if refresh_token is None:
+                raise SliceManagerException(f"Unable to refresh tokens: no refresh token found!")
             self.refresh_tokens(refresh_token=refresh_token)
 
     def get_refresh_token(self) -> str:
